6dc414f08000802f3392359469b0dfc31d30f522
adi <tiliztiadi@gmail.com>
2026-08-07 13:50:53 +0700

    fix: production force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Validator::extend('required_without_all', function ($attribute, $value, $parameters, $validator) {
             $data = $validator->getData();
             foreach ($parameters as $field) {
